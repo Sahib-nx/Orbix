446f4dd2e48d0a8c37c4a724a01372d9639d4ca2
Sahib-nx <sahibyar214@gmail.com>
2025-06-22 14:44:43 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 
 //Initialze socket.io server
 export const io = new Server(server, {
-    cors: {origin : "*"}
+    cors: { origin: "*" }
 })
 
 //Store online users
@@ -25,7 +25,7 @@ io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
     console.log("User Connected", userId);
 
-    if(userId) userSocketMap[userId] = socket.id;
+    if (userId) userSocketMap[userId] = socket.id;
 
     //Emit online users to all connected clients
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
@@ -38,7 +38,7 @@ io.on("connection", (socket) => {
 })
 
 // Middleware setup
-app.use(express.json({limit : "4mb"}));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 //Routes setup
@@ -49,5 +49,11 @@ app.use("/api/messages", messageRouter);
 //Connect to mongodb
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on Port:" + PORT));
+
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => console.log("Server is running on Port:" + PORT));
+}
+
+//export server for vercel
+export default server;
